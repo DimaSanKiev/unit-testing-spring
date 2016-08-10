05582e54bec7f7205177f3f78324e104fc3b2218
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-08-10 22:56:47 +0300

    add FavoriteController, LoginController, WeatherController

--- src/main/java/weather/web/FlashMessage.java
+++ src/main/java/weather/web/FlashMessage.java
@@ -2,7 +2,12 @@ package weather.web;
 
 public class FlashMessage {
     private String message;
-    private String status;
+    private Status status;
+
+    public FlashMessage(String message, Status status) {
+        this.message = message;
+        this.status = status;
+    }
 
     public static enum Status {
         SUCCESS,
@@ -18,11 +23,11 @@ public class FlashMessage {
         this.message = message;
     }
 
-    public String getStatus() {
+    public Status getStatus() {
         return status;
     }
 
-    public void setStatus(String status) {
+    public void setStatus(Status status) {
         this.status = status;
     }
 }
---
+++ src/main/java/weather/web/controller/WeatherController.java
@@ -0,0 +1,51 @@
+package weather.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import weather.service.GeocodingService;
+import weather.service.WeatherService;
+import weather.service.dto.geocoding.GeocodingResult;
+import weather.service.dto.geocoding.Location;
+import weather.service.dto.weather.Weather;
+
+import static weather.util.WebUtils.uriEncode;
+
+@Controller
+public class WeatherController {
+    @Autowired
+    private WeatherService weatherService;
+
+    @Autowired
+    private GeocodingService geocodingService;
+
+    @RequestMapping("/geo")
+    public String getWeatherLocation(@RequestParam double lat, @RequestParam double lng, Model model) {
+        Location loc = new Location(lat, lng);
+        Weather weather = weatherService.findByLocation(loc);
+        model.addAttribute("weather", weather);
+        return "weather/detail";
+    }
+
+    @RequestMapping("/search")
+    public String search(@RequestParam String q) {
+        return String.format("redirect:/search/%s", uriEncode(q));
+    }
+
+    @RequestMapping("/search/{q}")
+    public String getWeatherForSearchTerm(@PathVariable String q, Model model) {
+        GeocodingResult geocodingResult = geocodingService.findBySearchTerm(q);
+        Weather weather = weatherService.findByLocation(geocodingResult.getGeometry().getLocation());
+        model.addAttribute("geocodingResult", geocodingResult);
+        model.addAttribute("weather", weather);
+        return "weather/detail";
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "weather/detail";
+    }
+}
