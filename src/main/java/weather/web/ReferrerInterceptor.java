13de9699dddb1280bce3f2d2d2924e64f0756ec4
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-08-10 22:26:35 +0300

    add application configuration and data configuration

---
+++ src/main/java/weather/web/ReferrerInterceptor.java
@@ -0,0 +1,19 @@
+package weather.web;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ReferrerInterceptor extends HandlerInterceptorAdapter {
+    public static final String ATT_REFERRER_URI = "referrer";
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        request.getSession().setAttribute(ATT_REFERRER_URI, request.getRequestURI());
+    }
+
+    public static String redirect(HttpServletRequest req) {
+        return "redirect:" + req.getSession().getAttribute(ATT_REFERRER_URI);
+    }
+}
