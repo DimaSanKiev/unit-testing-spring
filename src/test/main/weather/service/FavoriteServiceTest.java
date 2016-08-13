fc2db8da00abff4dd40ebd111e08ac1dcbbd7bef
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-08-13 13:54:00 +0300

    testing FavoriteService methods

--- src/test/main/weather/service/FavoriteServiceTest.java
+++ src/test/main/weather/service/FavoriteServiceTest.java
@@ -6,8 +6,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import weather.dao.FavoriteDao;
+import weather.domain.Favorite;
+import weather.exception.FavoriteNotFoundException;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FavoriteServiceTest {
@@ -20,16 +28,27 @@ public class FavoriteServiceTest {
 
     @Test
     public void findAll_ShouldReturnTwo() throws Exception {
-        
+        List<Favorite> faves = Arrays.asList(
+                new Favorite(),
+                new Favorite()
+        );
+
+        when(dao.findAll()).thenReturn(faves);
+
+        assertEquals("findAll should return two favorites", 2, service.findAll().size());
+        verify(dao).findAll();
     }
 
     @Test
     public void findById_ShouldReturnOne() throws Exception {
-
+        when(dao.findOne(1L)).thenReturn(new Favorite());
+        assertThat(service.findById(1L), instanceOf(Favorite.class));
     }
 
-    @Test
+    @Test(expected = FavoriteNotFoundException.class)
     public void findById_ShouldThrowFavoriteNotFoundException() throws Exception {
-
+        when(dao.findOne(1L)).thenReturn(null);
+        service.findById(1L);
+        verify(dao).findOne(1L);
     }
 }
